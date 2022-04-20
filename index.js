d6df019599f00d83835509f993bffcfdcf26a9b8
vishalsg42 <vishalsg42@gmail.com>
2022-04-20 21:14:19 +0530

    added boilerplate

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
 
 exports.handler = async (event) => {
+  console.log("event", JSON.stringify(event, null,2));
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: JSON.stringify(
       {
         message: "Go Serverless v2.0! Your function executed successfully!",
